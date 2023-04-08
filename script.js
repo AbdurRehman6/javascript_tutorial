40c8fe52c13c0038c5b0c4c81549840d4e0a644d
Abdur Rehman <94692937+AbdurRehman6@users.noreply.github.com>
2023-04-08 17:56:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// variables , constants and comment
+
+// var score = 34;
+// var score = 50;  // redeclare
+// console.log(score)
+
+// aaj kal naye variables aaye hai
+// reassignment ho jata hai
+
+// let points = 6;  // we can't redeclare
+// console.log(points)
+
+// constants
+const value = 10;  // declare karte hi value deni hai
+console.log(value)
